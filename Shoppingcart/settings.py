b347c5ddd2ea1321bb2dd0cd4310961c6636b8a8
x22102817Raj <x22102817@student.ncirl.ie>
2023-04-24 20:58:51 +0000

    settings chnage

--- Shoppingcart/settings.py
+++ Shoppingcart/settings.py
@@ -108,12 +108,7 @@ if 'RDS_HOSTNAME' in os.environ:
         }
     }
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     }
-    # }
+
     
     DATABASES = {
         'default': {
